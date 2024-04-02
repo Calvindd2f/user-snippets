7fd0c27a716a34afc152d64d6ef4d0a853580c7a
Calvindd2f <calvinbergin212@gmail.com>
2024-04-02 02:14:35 +0100

    This is not a release commit so the workflows have not been created. It should not work so please to not rollback. It is just late.

--- main.ts
+++ main.ts
@@ -1,134 +1,161 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { promises as fs } from 'fs';
 
-// Remember to rename these classes and interfaces!
+const snippetsFile = './powershell-snippets.json';
+const snippets = JSON.parse(fs.readFile(snippetsFile, 'utf8'));
 
-interface MyPluginSettings {
-	mySetting: string;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+async function insertPowerShellSnippet(editor: Editor, snippet: any) {
+  editor.replaceSelection(snippet.body.join('\n'), 'around');
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+// The main class for the Obsidian plugin
+export default class CalvinsObsidianSnippets extends Plugin {
+  settings: PluginSettings;
+
+  extractLanguage = (source: string): string => {
+    const match = RegExp(/```(\w+)/).exec(source);
+    return match? match[1].toLowerCase() : '';
+  }
+
+  findMatchingSnippet = (language: string): string | undefined => {
+    return snippets.find((snippet: { language: string; }) => snippet.language === language)?.body.join('\n');
+  }
+
+  // Open the modal with the available snippets for the given language
+  async openModal(options: string[], view: MarkdownView): Promise<void> {
+    const { contentEl, titleEl, toggleButtonEl } = new Modal(this.app);
+    titleEl.setText('Select Snippet');
+    toggleButtonEl.setText('Cancel');
+
+    for (const option of options) {
+      const button = this.createButton(option);
+      button.addEventListener('click', () => {
+        const matchingSnippet = this.findMatchingSnippet(option);
+        if (matchingSnippet) {
+          this.insertSnippet(view.editor, matchingSnippet);
+        }
+        this.closeModal();
+      });
+      contentEl.appendChild(button);
+    }
+
+    this.registerModal(titleEl, contentEl);
+  }
+	closeModal() {
+		throw new Error('Method not implemented.');
+	}
+	registerModal(titleEl: HTMLElement, contentEl: HTMLElement) {
+		throw new Error('Method not implemented.');
+	}
+
+  async insertSnippet(editor: Editor, snippet: string): Promise<void> {
+    const selection = editor.getSelection();
+    const position = editor.getCursor();
+    editor.replaceSelection(snippet, 'around');
+    editor.setSelection(position.line, selection.start, position.line, selection.end);
+  }
+
+  createButton = (text: string): HTMLButtonElement => {
+    const button = document.createElement('button');
+    button.classList.add('button', 'button-small');
+    button.textContent = text;
+    return button;
+  }
+
+  // Load settings when the plugin is initialized
+  async loadSettings() {
+    this.settings = { ...DEFAULT_SETTINGS, ...await this.loadData()};
+  }
+
+  // Save settings when the plugin is unloaded
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
+
+  // The onLoad method is called when the plugin is loaded
+  async onload() {
+    await this.loadSettings();
+
+    // Add a ribbon icon to the Obsidian interface
+    this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+      new Notice('This is a notice!');
+    });
+
+    // Add a status bar item to the Obsidian interface
+    this.addStatusBarItem().setText('Status Bar Text');
+
+    // Define a command to insert a snippet into the editor
+    this.addCommand({
+      id: 'insert-snippet',
+      name: 'Insert Snippet',
+      // Define the editor callback for the command
+      editorCallback: async (editor: Editor, view: MarkdownView) => {
+        const selection = editor.getSelection();
+        const language = this.extractLanguage(selection);
+        if (!language) {
+          new Notice('Please select a code block with a language identifier');
+          return;
+        }
+        const matchingSnippet = this.findMatchingSnippet(language);
+        if (matchingSnippet) {
+          await this.insertSnippet(editor, matchingSnippet);
+        } else {
+          new Notice(`No snippet found for language: ${language}`);
+        }
+      },
+      // Define the view callback for the command
+      viewCallback: (view: MarkdownView) => {
+        const position = view.getCursor();
+        const language = this.extractLanguage(view.getLine(position.line));
+        if (language) {
+          const options = snippets.filter((snippet: { language: string; }) => snippet.language === language).map((snippet: { name: unknown; }) => snippet.name);
+          return this.openModal(options, view);
+        }
+      }
+    });
+
+    // Register the plugin settings tab
+    this.addSettingTab(new SampleSettingTab(this.app, this));
+  }
+
+  // The onunload method is called when the plugin is unloaded
+  onunload() {
+    console.log('unloading plugin');
+  }
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+interface PluginSettings {
+  mySetting: string;
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+const DEFAULT_SETTINGS: PluginSettings = {
+  mySetting: 'default'
+};
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
+// The settings tab for the plugin
 class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+  plugin: CalvinsObsidianSnippets;
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+  constructor(app: App, plugin: CalvinsObsidianSnippets) {
+    super(app, plugin);
+    this.plugin = plugin;
+  }
 
-	display(): void {
-		const {containerEl} = this;
+  display(): void {
+    const { containerEl } = this;
 
-		containerEl.empty();
+    containerEl.empty();
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+    // Add a setting to the settings tab
+    new Setting(containerEl)
+     .setName('Setting #1')
+     .setDesc('This is a description for setting #1')
+     .addText(text => text.setPlaceholder('Enter something')
+      .setValue(this.plugin.settings.mySetting)
+      .onChange(async (value) => {
+        this.plugin.settings.mySetting = value;
+        await this.plugin.saveSettings();
+      }));
+  }
 }
